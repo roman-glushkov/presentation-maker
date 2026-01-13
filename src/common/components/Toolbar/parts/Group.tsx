d01bf5650298e76d6d86d3b8b385562a9bf30318
roman-glushkov <roma.glushkov.vuz@mail.ru>
2026-01-13 18:45:12 +0300

    коонечная демка, ну я надеюсь

--- src/common/components/Toolbar/parts/Group.tsx
+++ src/common/components/Toolbar/parts/Group.tsx
@@ -4,10 +4,7 @@ import { handleAction, addImageWithUrl } from '../../../../store/editorSlice';
 import { setActiveTextOption } from '../../../../store/toolbarSlice';
 import { GROUPS, GroupButton, GroupKey } from '../constants/config';
 import { useNotifications } from '../../../../services/hooks/useNotifications';
-import {
-  IMAGE_NOTIFICATIONS,
-  NOTIFICATION_TIMEOUT,
-} from '../../../../services/notifications/messages';
+import { IMAGE_NOTIFICATIONS, NOTIFICATION_TIMEOUT } from '../../../../services/notifications';
 import { ImageService } from '../../../../services/services/ImageService';
 import { RootState } from '../../../../store';
 
