2aebeb79d48545b3aac72b5a0251c90575cc90ab
roman-glushkov <roma.glushkov.vuz@mail.ru>
2026-01-21 14:01:36 +0300

    убрал фичу, которая ломает все

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,38 +1,8 @@
-import { configureStore, Middleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import editorReducer from './editorSlice';
 import toolbarReducer from './toolbarSlice';
-const localStorageMiddleware: Middleware = (store) => (next) => (action) => {
-  const result = next(action);
-  const state = store.getState();
-
-  localStorage.setItem('editorState', JSON.stringify(state.editor));
-
-  return result;
-};
-const loadState = () => {
-  try {
-    const serializedState = localStorage.getItem('editorState');
-    if (serializedState === null) {
-      return undefined;
-    }
-    const parsedState = JSON.parse(serializedState);
-    if (parsedState && typeof parsedState === 'object') {
-      return parsedState;
-    }
-
-    return undefined;
-  } catch (err) {
-    console.error(err);
-    return undefined;
-  }
-};
-
-const preloadedState = {
-  editor: loadState(),
-};
 
 export const store = configureStore({
-  preloadedState,
   reducer: {
     editor: editorReducer,
     toolbar: toolbarReducer,
@@ -40,7 +10,7 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }).concat(localStorageMiddleware),
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
